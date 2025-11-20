b97f3547322626105bd73a9fdf6ce627e47e4216
deepak1556 <hop2deep@gmail.com>
2025-11-20 23:05:18 +0900

    chore: bump explorer-command

--- build/win32/explorer-dll-fetcher.js
+++ build/win32/explorer-dll-fetcher.js
@@ -37,12 +37,12 @@ async function downloadExplorerDll(outDir, quality = 'stable', targetArch = 'x64
     d(`downloading ${fileName}`);
     const artifact = await (0, get_1.downloadArtifact)({
         isGeneric: true,
-        version: 'v4.0.0-355426',
+        version: 'v5.0.0-377200',
         artifactName: fileName,
         checksums,
         mirrorOptions: {
             mirror: 'https://github.com/microsoft/vscode-explorer-command/releases/download/',
-            customDir: 'v4.0.0-355426',
+            customDir: 'v5.0.0-377200',
             customFilename: fileName
         }
     });
