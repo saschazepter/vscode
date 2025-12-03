e14c07e17bd733ee43abd90ad6e2e4cc34735a6a
Josh Spicer <23246594+joshspicer@users.noreply.github.com>
2025-12-03 15:33:22 -0800

    guard chatSession contributions with canDelegate

--- src/vs/workbench/contrib/chat/browser/chatSessions.contribution.ts
+++ src/vs/workbench/contrib/chat/browser/chatSessions.contribution.ts
@@ -676,10 +676,11 @@ export class ChatSessionsService extends Disposable implements IChatSessionsServ
 	private _enableContribution(contribution: IChatSessionsExtensionPoint, ext: IRelaxedExtensionDescription): void {
 		const disposableStore = new DisposableStore();
 		this._contributionDisposables.set(contribution.type, disposableStore);
-
-		disposableStore.add(this._registerAgent(contribution, ext));
-		disposableStore.add(this._registerCommands(contribution));
-		disposableStore.add(this._registerMenuItems(contribution, ext));
+		if (contribution.canDelegate) {
+			disposableStore.add(this._registerAgent(contribution, ext));
+			disposableStore.add(this._registerCommands(contribution));
+			disposableStore.add(this._registerMenuItems(contribution, ext));
+		}
 	}
 
 	private _disposeSessionsForContribution(contributionId: string): void {
