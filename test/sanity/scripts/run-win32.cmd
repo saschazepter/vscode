05bf561520cd62800a03be943c98246344232532
Dmitriy Vasyura <dmitriv@microsoft.com>
2026-01-29 13:45:29 -0800

    Updates

--- test/sanity/scripts/run-win32.cmd
+++ test/sanity/scripts/run-win32.cmd
@@ -8,6 +8,7 @@ echo System: %OS% %ARCH_NAME%
 powershell -NoProfile -Command "$mem = (Get-CimInstance Win32_ComputerSystem).TotalPhysicalMemory; Write-Host ('Memory: {0:N0} GB' -f ($mem/1GB))"
 powershell -NoProfile -Command "$disk = Get-PSDrive C; Write-Host ('Disk C: {0:N0} GB free of {1:N0} GB' -f ($disk.Free/1GB), (($disk.Used+$disk.Free)/1GB))"
 
+echo Checking if WSL is installed
 where wsl >nul 2>nul
 if errorlevel 1 call :install_wsl_feature
 
@@ -18,12 +19,14 @@ powershell -NoProfile -Command "if ((wsl -l -q) -contains 'Ubuntu') { exit 0 } e
 if errorlevel 1 call :install_ubuntu_image
 
 :run_tests
+
 set PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
 set PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH=C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe
 
 echo Running sanity tests
 node "%~dp0..\out\index.js" %*
-exit /b %ERRORLEVEL%
+
+goto :eof
 
 :install_ubuntu_image
 
