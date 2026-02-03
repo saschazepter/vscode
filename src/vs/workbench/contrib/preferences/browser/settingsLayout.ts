d226ed53359ed9e80d13362846e7d4e712ad1528
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2026-02-02 16:04:04 -0800

    Fix extension reference

--- src/vs/workbench/contrib/preferences/browser/settingsLayout.ts
+++ src/vs/workbench/contrib/preferences/browser/settingsLayout.ts
@@ -31,7 +31,7 @@ const COMMONLY_USED_SETTINGS: readonly string[] = [
 	'editor.fontSize',
 	'editor.formatOnSave',
 	'files.autoSave',
-	'GitHub.copilot.manageExtension',
+	'GitHub.copilot-chat.manageExtension',
 	'editor.defaultFormatter',
 	'editor.fontFamily',
 	'editor.wordWrap',
