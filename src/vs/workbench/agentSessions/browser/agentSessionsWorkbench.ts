7f2777222e1a06161ec811ad9d48cd420203051d
Sandeep Somavarapu <sasomava@microsoft.com>
2026-02-04 17:09:29 +0100

    fix acquiring insta service in config service

--- src/vs/workbench/agentSessions/browser/agentSessionsWorkbench.ts
+++ src/vs/workbench/agentSessions/browser/agentSessionsWorkbench.ts
@@ -390,6 +390,13 @@ export class AgentSessionsWorkbench extends Disposable implements IWorkbenchLayo
 		instantiationService.invokeFunction(accessor => {
 			const lifecycleService = accessor.get(ILifecycleService);
 
+			// TODO@Sandeep debt around cyclic dependencies
+			const configurationService = accessor.get(IConfigurationService);
+			// eslint-disable-next-line local/code-no-in-operator
+			if (configurationService && 'acquireInstantiationService' in configurationService) {
+				(configurationService as { acquireInstantiationService: (instantiationService: unknown) => void }).acquireInstantiationService(instantiationService);
+			}
+
 			// Signal to lifecycle that services are set
 			lifecycleService.phase = LifecyclePhase.Ready;
 		});
