bd8d4c419f5f4e9a9d8c58648172bfe7d5a029f8
Benjamin Pasero <benjamin.pasero@microsoft.com>
2026-02-05 17:26:22 +0100

    Revert "ignore watch errors (#292873)"
    This reverts commit 08f39979571b5db7a557099e5be85bffd291831e.

--- build/lib/watch/index.ts
+++ build/lib/watch/index.ts
@@ -34,7 +34,12 @@ const subscriptionCache: Map<string, { stream: Stream; subscription: Promise<wat
 function createWatcher(root: string): Stream {
 	const result = es.through();
 
-	const subscription = watcher.subscribe(root, (_err, events) => {
+	const subscription = watcher.subscribe(root, (err, events) => {
+		if (err) {
+			result.emit('error', err);
+			return;
+		}
+
 		for (const event of events) {
 			const relativePath = path.relative(root, event.path);
 
@@ -56,6 +61,8 @@ function createWatcher(root: string): Stream {
 			'**/out/**'
 		]
 	});
+
+	subscription.catch(err => result.emit('error', err));
 
 	// Cleanup on process exit
 	const cleanup = () => {
