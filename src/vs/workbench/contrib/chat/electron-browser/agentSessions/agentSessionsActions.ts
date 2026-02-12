b45d6ae4da5df307cebed9d44b34358d97a8a6df
Sandeep Somavarapu <sasomava@microsoft.com>
2026-02-12 18:27:49 +0100

    rename agent sessions

--- src/vs/workbench/contrib/chat/electron-browser/agentSessions/agentSessionsActions.ts
+++ src/vs/workbench/contrib/chat/electron-browser/agentSessions/agentSessionsActions.ts
@@ -8,16 +8,14 @@ import { Action2 } from '../../../../../platform/actions/common/actions.js';
 import { ContextKeyExpr } from '../../../../../platform/contextkey/common/contextkey.js';
 import { INativeHostService } from '../../../../../platform/native/common/native.js';
 import { ChatEntitlementContextKeys } from '../../../../services/chat/common/chatEntitlementService.js';
-import { IWorkbenchModeService } from '../../../../services/layout/common/workbenchModeService.js';
-import { IsAgentSessionsWorkspaceContext, WorkbenchModeContext } from '../../../../common/contextkeys.js';
 import { CHAT_CATEGORY } from '../../browser/actions/chatActions.js';
 import { ProductQualityContext } from '../../../../../platform/contextkey/common/contextkeys.js';
 
-export class OpenAgentSessionsWindowAction extends Action2 {
+export class SessionsWindowAction extends Action2 {
 	constructor() {
 		super({
-			id: 'workbench.action.openAgentSessionsWindow',
-			title: localize2('openAgentSessionsWindow', "Open Agent Sessions Window"),
+			id: 'workbench.action.openSessionsWindow',
+			title: localize2('openSessionsWindow', "Open Sessions Window"),
 			category: CHAT_CATEGORY,
 			precondition: ContextKeyExpr.and(ProductQualityContext.notEqualsTo('stable'), ChatEntitlementContextKeys.Setup.hidden.negate()),
 			f1: true,
@@ -26,51 +24,6 @@ export class OpenAgentSessionsWindowAction extends Action2 {
 
 	async run(accessor: ServicesAccessor) {
 		const nativeHostService = accessor.get(INativeHostService);
-
 		await nativeHostService.openSessionsWindow();
-	}
-}
-
-export class SwitchToAgentSessionsModeAction extends Action2 {
-	constructor() {
-		super({
-			id: 'workbench.action.switchToAgentSessionsMode',
-			title: localize2('switchToAgentSessionsMode', "Switch to Agent Sessions Mode"),
-			category: CHAT_CATEGORY,
-			precondition: ContextKeyExpr.and(
-				ProductQualityContext.notEqualsTo('stable'),
-				ChatEntitlementContextKeys.Setup.hidden.negate(),
-				IsAgentSessionsWorkspaceContext.toNegated(),
-				WorkbenchModeContext.notEqualsTo('agent-sessions')
-			),
-			f1: true,
-		});
-	}
-
-	async run(accessor: ServicesAccessor) {
-		const workbenchModeService = accessor.get(IWorkbenchModeService);
-		await workbenchModeService.setWorkbenchMode('agent-sessions');
-	}
-}
-
-export class SwitchToNormalModeAction extends Action2 {
-	constructor() {
-		super({
-			id: 'workbench.action.switchToNormalMode',
-			title: localize2('switchToNormalMode', "Switch to Default Mode"),
-			category: CHAT_CATEGORY,
-			precondition: ContextKeyExpr.and(
-				ProductQualityContext.notEqualsTo('stable'),
-				ChatEntitlementContextKeys.Setup.hidden.negate(),
-				IsAgentSessionsWorkspaceContext.toNegated(),
-				WorkbenchModeContext.notEqualsTo('')
-			),
-			f1: true,
-		});
-	}
-
-	async run(accessor: ServicesAccessor) {
-		const workbenchModeService = accessor.get(IWorkbenchModeService);
-		await workbenchModeService.setWorkbenchMode(undefined);
 	}
 }
