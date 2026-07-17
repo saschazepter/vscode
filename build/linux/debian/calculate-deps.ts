ea8a24bd156dfdf3afd8f02246933cbd90d69d9b
deepak1556 <hop2deep@gmail.com>
2026-07-17 16:03:00 +0900

    fix: resolve binary path wrt sysroot

--- build/linux/debian/calculate-deps.ts
+++ build/linux/debian/calculate-deps.ts
@@ -61,7 +61,7 @@ function calculatePackageDeps(binaryPath: string, arch: DebianArchString, chromi
 			break;
 	}
 	cmd.push(`-l${chromiumSysroot}/usr/lib`);
-	cmd.push(`-l${path.dirname(binaryPath)}`);
+	cmd.push(`-l${path.dirname(path.resolve(binaryPath))}`);
 	cmd.push(`-L${vscodeSysroot}/debian/libxkbfile1/DEBIAN/shlibs`);
 	cmd.push('-O', '-e', path.resolve(binaryPath));
 
